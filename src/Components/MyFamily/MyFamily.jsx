a966ad5d76f1c4768bfe796f31a8ce1a4846170c
rachit gour <rs3501194@gmail.com>
2025-03-25 15:45:53 +0530

    commit

--- src/Components/MyFamily/MyFamily.jsx
+++ src/Components/MyFamily/MyFamily.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
-import "./Myfamily.css";
+import './MyFamily.css'
 import { useDispatch, useSelector } from "react-redux";
 import { message } from "antd";
 import { assets } from "../../assets/assets";
